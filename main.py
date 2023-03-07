a77270d645c23fc50805210161bf9e28aad32cd7
jk1 <jakub.kos212@gmail.com>
2023-03-07 18:19:45 +0100

    added post class basic stuff

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 from fastapi import FastAPI
+import requests
+from starlette.responses import FileResponse
+import json
+
+import post
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
-
+    return FileResponse('templates/index.html')
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+@app.get("/test/")
+async def test():
+    return testpost.test_func()
+
+response = requests.get("https://jsonplaceholder.typicode.com/posts")
+res = json.loads(response.content)
+
+
+temp = dict(res[1])
+testpost = post.Post(temp["userId"], temp["id"], temp["title"], temp["body"])
+
---
+++ post.py
@@ -0,0 +1,6 @@
+class Post:
+    def __init__(self, userId, id, title, body):
+        self.userId = userId
+        self.id = id
+        self.title = title
+        self.body = body
