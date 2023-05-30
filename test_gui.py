14920a0a7ce37f7eb1993382eb3f9dbd183b862c
jk1 <jakub.kos212@gmail.com>
2023-05-30 17:14:59 +0200

    added docstrings in test_gui.py

--- test_gui.py
+++ test_gui.py
@@ -1,8 +1,9 @@
-import re
+"""GUI tests for the web app"""
 from playwright.sync_api import Page, expect
 
 
-def test_homepage_has_Fast_Api_Project_in_title(page: Page):
+def test_homepage_has_fast_api_project_in_title(page: Page):
+    """checks if starting page has correct title"""
     page.goto("http://16.170.222.98/")
 
     # Expect a title "to contain" a substring.
@@ -10,6 +11,7 @@ def test_homepage_has_Fast_Api_Project_in_title(page: Page):
 
 
 def test_homepage_has_users_button(page: Page):
+    """checks if button "użytkownicy" is clickable"""
     page.goto("http://16.170.222.98/")
 
     # Expect a title "to contain" a substring.
@@ -19,6 +21,7 @@ def test_homepage_has_users_button(page: Page):
 
 
 def test_homepage_has_mainpage_button(page: Page):
+    """checks if button "mainpage" is clickable"""
     page.goto("http://16.170.222.98/")
 
     # Expect a title "to contain" a substring.
@@ -28,6 +31,7 @@ def test_homepage_has_mainpage_button(page: Page):
 
 
 def test_homepage_has_posts_button(page: Page):
+    """checks if button "posty" is clickable"""
     page.goto("http://16.170.222.98/")
 
     # Expect a title "to contain" a substring.
@@ -37,18 +41,21 @@ def test_homepage_has_posts_button(page: Page):
 
 
 def test_posts_has_title(page: Page):
+    """checks if page "posts" has correct title"""
     page.goto("http://16.170.222.98/posts")
 
     expect(page).to_have_title("Fast Api Project")
 
 
 def test_users_has_title(page: Page):
+    """checks if page "users" has correct titles"""
     page.goto("http://16.170.222.98/users")
 
     expect(page).to_have_title("Fast Api Project")
 
 
 def test_post1_has_title(page: Page):
+    """checks if post 1 has correct title"""
     page.goto("http://16.170.222.98/mainpage/post/1")
 
     expect(page).to_have_title("Fast Api Project")
